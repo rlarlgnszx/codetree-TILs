4ed7bf9fa7d31dde584a0ff4a3aa0f8660d959ea
Kihoon KIM <40743105+rlarlgnszx@users.noreply.github.com>
2024-10-24 21:22:58 +0900

    Title: 스승의 은혜, Time: 135ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241024/스승의 은혜/the-grace-form-teacher.py
@@ -0,0 +1,21 @@
+import sys
+input =sys.stdin.readline
+
+n,k = map(int,input().split())
+temp = [list(map(int,input().split())) for j in range(n)]
+temp.sort()
+temp.sort(key=lambda x:x[1])
+ans = 0
+ticket = 1
+for a,b in temp:
+    if k-(a+b) <0 and ticket:
+        ticket-=1
+        if k-(a//2)-b >=0:
+            ans+=1
+            k-= ((a//2)+b)
+        else:
+            break
+    elif k-(a+b)>=0:
+        k-= (a+b)
+        ans+=1
+print(ans)
