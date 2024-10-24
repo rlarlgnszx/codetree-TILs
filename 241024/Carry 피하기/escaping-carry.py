a9ec6beab77356e523789c99d4407a45d4ee4ec8
Kihoon KIM <40743105+rlarlgnszx@users.noreply.github.com>
2024-10-24 19:07:58 +0900

    Title: Carry 피하기, Time: 198ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 241024/Carry 피하기/escaping-carry.py
+++ 241024/Carry 피하기/escaping-carry.py
@@ -9,31 +9,31 @@ def is_valid(stack, num):
                 return False
     return True
 
+def dfs(index, selected, nums, max_count):
+    # 현재 index에서 선택한 숫자들로 탐색을 진행하는 함수
+    max_count[0] = max(max_count[0], len(selected))  # 최댓값 갱신
+    
+    if index == len(nums):
+        return
+    
+    # 현재 숫자를 선택하는 경우
+    if is_valid(selected, nums[index]):
+        selected.append(nums[index])
+        dfs(index + 1, selected, nums, max_count)  # 다음 숫자 탐색
+        selected.pop()  # 선택을 취소하고 다른 선택을 시도
+    
+    # 현재 숫자를 선택하지 않는 경우
+    dfs(index + 1, selected, nums, max_count)
+
 def solve():
     n = int(input())  # 숫자의 개수
     nums = [int(input()) for _ in range(n)]  # 주어진 숫자 리스트
     
-    stack = []
-    max_count = 0
-    i = 0
+    max_count = [0]  # 최대 개수를 저장할 리스트 (mutable로 전달하기 위해 리스트 사용)
     
-    while True:
-        if i == n:
-            # 모든 숫자를 처리한 후, 최대 개수를 기록하고 돌아감
-            max_count = max(max_count, len(stack))
-            if not stack:  # 스택이 비어있다면 종료
-                break
-            # 스택에서 하나를 빼고 그 위치로 돌아가서 다른 선택을 시도
-            i = stack.pop()[1] + 1
-        elif is_valid([x[0] for x in stack], nums[i]):
-            # 스택에 현재 숫자를 추가 (숫자와 그 인덱스 기록)
-            stack.append((nums[i], i))
-            i += 1
-        else:
-            # 현재 숫자를 스택에 추가하지 않고 다음 숫자로 넘어감
-            i += 1
-
-    print(max_count)
+    dfs(0, [], nums, max_count)  # DFS 탐색 시작
+    
+    print(max_count[0])
 
 # 테스트 실행
 solve()
